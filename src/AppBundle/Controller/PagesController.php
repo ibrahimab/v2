7b0cc7c27b4cb4c14eb7a6ef488cedb322ee00e7
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-03-06 14:27:51 +0000

    Implemented first part of the home page + new Pages Controller + first steps of implementing highlights on home page

--- src/AppBundle/Controller/PagesController.php
+++ src/AppBundle/Controller/PagesController.php
@@ -5,13 +5,13 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class PagesController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
