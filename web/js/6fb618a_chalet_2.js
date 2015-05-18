4276e4903bd01e05380e6c1fafa04b203b1b4221
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-05-18 15:47:59 +0200

    New asset recompilation for push to staging environment

--- web/js/6fb618a_chalet_2.js
+++ web/js/6fb618a_chalet_2.js
@@ -1,25 +1,47 @@
+if (typeof Object.create !== 'function') {
+
+    Object.create = function(o, props) {
+
+        function F() {};
+        F.prototype = o;
+        result = new F();
+
+        if (typeof(props) === 'object') {
+
+            for (prop in props) {
+
+                if (props.hasOwnProperty(prop)) {
+                    result[prop] = props[prop].value;
+                }
+            }
+        }
+
+        return result;
+    };
+}
+
 /**
  * Chalet Object
  *
  * @author Ibrahim Abdullah <ibrahim@chalet.nl>
  */
-(function(ns, undefined) {
+(function(Chalet, undefined) {
     // creating new 'strict' namespace to keep global pollution
     // to a minimum. Code inside here can reference the namespace using the 'ns' object
     'use strict';
-    
+
     // @property public
-    ns.attributes = {};
-    
+    Chalet.attributes = {};
+
     // @method import - importing attributes
-    ns.import = function(attributes) {
+    Chalet.import = function(attributes) {
         this.attributes  = attributes;
     };
-    
+
     // getting reference to internal storage
     // for easy access
-    ns.get = function() {
+    Chalet.get = function() {
         return this.attributes;
     };
-    
+
 })(window.Chalet = window.Chalet || {});
