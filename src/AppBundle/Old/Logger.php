a941692f5d4492b56eded4c140d5d81d674f16cd
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-06-22 17:41:43 +0200

    [RES-28] Created Logger class for new website

---
+++ src/AppBundle/Old/Logger.php
@@ -0,0 +1,50 @@
+<?php
+namespace AppBundle\Old;
+use       \ChaletLoggerInterface as LoggerInterface;
+
+class Logger implements LoggerInterface
+{
+    /**
+     * @var string
+     */
+    private $collector;
+    
+    /**
+     * @param string $collector
+     */
+    public function __construct($collector = 'messages')
+    {
+        $this->setCollector($collector);
+    }
+    
+    /**
+     * @param string $collector
+     * @return LoggerInterface
+     */
+    public function setCollector($collector)
+    {
+        $this->logger = $collector;
+        
+        return $this;
+    }
+    
+    /**
+     * @param string $message
+     * @return LoggerInterface
+     */
+    public function error($message)
+    {
+        return $this->log($message, 'error');
+    }
+    
+    /**
+     * @param string $message
+     * @param string $label
+     * @return LoggerInterface
+     */
+    public function log($message, $label = 'default')
+    {
+        dump($label, $message);
+        return $this;
+    }
+}
