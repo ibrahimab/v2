7ef41b1190fd1be8321e4818f42c8a09b50e4715
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-07-02 12:44:55 +0200

    [RES-5] Contact form and mailer completed

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Controller;
 use       AppBundle\Annotation\Breadcrumb;
+use       AppBundle\Entity\Form\Contact;
 use       Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use       Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use       Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -27,15 +28,56 @@ class ContactController extends Controller
         return $this->render('contact/new.html.twig', [
 
             'website_concern' => $this->get('app.concern.website'),
+            'form'            => [
+
+                'errors'  => [],
+                'contact' => [
+
+                    'name'        => '',
+                    'email'       => '',
+                    'phonenumber' => '',
+                    'message'     => '',
+                ],
+            ],
         ]);
     }
 
     /**
-     * @Route("/contact", name="create_contact")
+     * @Route("/contact.php", name="create_contact")
      * @Method("POST")
      */
-    public function create()
+    public function create(Request $request)
     {
-        return [];
+        $contact = new Contact($request->get('contact'));
+        $contact->validate();
+
+        if ($contact->isValid()) {
+
+            $mailer = $this->get('app.mailer.contact');
+            $result = $mailer->setSubject($this->get('translator')->trans('form-contact-subject'))
+                             ->setFrom($this->container->getParameter('mailer_from'))
+                             ->setTo($this->container->getParameter('mailer_to'))
+                             ->setTemplate('mail/contact.html.twig', 'text/html')
+                             ->setTemplate('mail/contact.txt.twig', 'text/plain')
+                             ->send($contact->getData());
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/new.html.twig', [
+
+            'website_concern' => $this->get('app.concern.website'),
+            'form'            => [
+
+                'errors'  => $contact->getErrors(),
+                'contact' => [
+
+                    'name'        => $contact->getName(),
+                    'email'       => $contact->getEmail(),
+                    'phonenumber' => $contact->getPhonenumber(),
+                    'message'     => $contact->getMessage(),
+                ],
+            ],
+        ]);
     }
 }
