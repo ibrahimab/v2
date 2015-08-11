d9bbc429a0578d67633f6615cef8b5002f20d3fd
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-08-11 15:53:49 +0200

    [RES-42] removed merge conflict mistake

--- src/AppBundle/Entity/Price/PriceRepository.php
+++ src/AppBundle/Entity/Price/PriceRepository.php
@@ -172,7 +172,7 @@ class PriceRepository extends BaseRepository implements PriceServiceRepositoryIn
         foreach ($results as $result) {
 
             $data[] = [
-                
+
                 'id'            => $result['type_id'],
                 'offer'         => (1 === (int)$result['kortingactief'] && 1 === (int)$result['aanbiedingskleur_korting']),
                 'price'         => floatval($result['prijs']),
@@ -254,7 +254,7 @@ class PriceRepository extends BaseRepository implements PriceServiceRepositoryIn
             if (!isset($data[$result['type_id']])) {
 
                 $data[$result['type_id']] = [
-                
+
                     'id'            => $result['type_id'],
                     'offer'         => (1 === (int)$result['kortingactief'] && 1 === (int)$result['aanbiedingskleur_korting']),
                     'prices'        => [],
@@ -282,24 +282,11 @@ class PriceRepository extends BaseRepository implements PriceServiceRepositoryIn
         return array_merge($arrangements, $accommodations);
     }
 
-    public function getArrangementDataByWeekendAndPersons($weekend, $persons)
-    {
-        return $this->getArrangementDataBy($weekend, $persons);
-    }
-
-    public function getDataByWeekendAndPersons($weekend, $persons)
-    {
-        $arrangements   = $this->getArrangementDataByWeekendAndPersons($weekend, $persons);
-        $accommodations = $this->getAccommodationDataByWeekend($weekend);
-
-        return array_merge($arrangements, $accommodations);
-    }
-    
     public function getDataByWeekendAndPersons($weekend, $persons)
     {
         $arrangements   = $this->getArrangementDataBy($weekend, $persons);
         $accommodations = $this->getAccommodationDataByPersons($persons);
-        
+
         return array_merge($arrangements, $accommodations);
     }
 }
