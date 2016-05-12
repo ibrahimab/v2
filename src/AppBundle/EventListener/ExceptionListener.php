188124685948218fb10ab0a51e83adcd8bf66f78
Ibrahim <abdul.ibrahim16@gmail.com>
2016-05-12 16:27:28 +0200

    [hotfix/res-185]
    * added custom 404 and 500 pages
    * redirect to search if path count is 1

---
+++ src/AppBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,59 @@
+<?php
+namespace AppBundle\EventListener;
+
+use AppBundle\Concern\LocaleConcern;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class ExceptionListener
+{
+    /**
+     *@var Router
+     */
+    private $router;
+
+    /**
+     *@var LocaleConcern
+     */
+    private $locale;
+
+    /**
+     * @param Router
+     */
+    public function __construct(Router $router, LocaleConcern $locale)
+    {
+        $this->router = $router;
+        $this->locale = $locale;
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     *
+     * @return void
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        $request   = $event->getRequest();
+
+        if ($exception instanceof NotFoundHttpException) {
+
+            $path  = $request->getRequestUri();
+            $parts = explode('/', $path);
+            $parts = array_filter($parts, function($part) {
+                return $part !== '';
+            });
+
+            if (count($parts) === 1) {
+
+                $locale = $this->locale->get();
+                $url    = $this->router->generate('search_' . $locale, ['fs' => current($parts)]);
+
+                $response = new RedirectResponse($url);
+                $event->setResponse($response);
+            }
+        }
+    }
+}
