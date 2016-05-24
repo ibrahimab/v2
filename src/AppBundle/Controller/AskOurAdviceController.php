b5b09e97696d476788aab39b1973f5f1c04bebbf
Jeroen Boschman <jeroen@webtastic.nl>
2016-05-24 21:22:10 +0200

    [feature/res-205] iframe form ask our advice

--- src/AppBundle/Controller/AskOurAdviceController.php
+++ src/AppBundle/Controller/AskOurAdviceController.php
@@ -11,15 +11,19 @@ use       Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @author  Ibrahim Abdullah <ibrahim@chalet.nl>
  * @package Chalet
  *
- * @Breadcrumb(name="ask_our_advice", title="ask_our_advice", translate=true, path="ask_our_advice")
+ * @Breadcrumb(name="frontpage", title="frontpage", translate=true, path="home")
  */
 class AskOurAdviceController extends Controller
 {
     /**
      * @Route("/vraag-ons-advies.php", name="ask_our_advice_nl")
      * @Route("/ask-our-advice.php", name="ask_our_advice_en")
+     * @Breadcrumb(name="ask_our_advice", title="Vraag ons advies", translate=true, path="ask_our_advice", active=true)
      */
     public function index()
     {
+        return $this->render('ask-our-advice/index.html.twig', [
+
+        ]);
     }
 }
